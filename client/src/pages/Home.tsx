431fba6f757c9af37332001ee95eae866efe8f90
Kevin Jonathan <realkevinjonathan@gmail.com>
2025-06-03 12:44:03 +0700

    skibidi

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,6 +5,11 @@ import Chatbot from '../components/Chatbot'
 
 import { useNavigate } from 'react-router-dom'
 import CertificationObject from '../components/CertificationObject'
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSquareFacebook, faSquareInstagram, faSquareXTwitter, faSquareYoutube } from "@fortawesome/free-brands-svg-icons"
+import melindaJpg from "../assets/melinda.jpg"
+import homeJumbotron from "../assets/home-jumbotron.png"
 
 function Home() {
     const certifications = [
@@ -18,14 +23,14 @@ function Home() {
             img: "src/assets/logo-damonsmile.png",
             title: "Certified Damon System Clinic",
             subtitle: "At Dentalign, we also specialize in Damon System treatment for all our customers.",
-            desc: "The Damon System is not just about revolutionary braces and wires, it’s a whole new way of treating patients. Traditional treatment often requires removal of healthy teeth and/or the use of palatal expanders to make space. This approach is often uncomfortable, takes longer, and can leave a narrower arch and a flat profile. Damon smiles are full, natural 10-tooth smiles achieved with light biologically-sensible forces, and are specifically designed to improve the overall facial result of each patient.",
+            desc: "The Damon System is not just about revolutionary braces and wires, it's a whole new way of treating patients. Traditional treatment often requires removal of healthy teeth and/or the use of palatal expanders to make space. This approach is often uncomfortable, takes longer, and can leave a narrower arch and a flat profile. Damon smiles are full, natural 10-tooth smiles achieved with light biologically-sensible forces, and are specifically designed to improve the overall facial result of each patient.",
         },
         
     ]
 
     const nav = useNavigate()
     const styles = {
-        jumbotron: "bg-[url(/src/assets/home-jumbotron.png)] bg-no-repeat bg-cover bg-center text-white min-h-150 flex items-center",
+        jumbotron: `bg-no-repeat bg-cover bg-center text-white min-h-150 flex items-center`,
         jumbo_content: "bg-[#00000075] flex flex-col items-start flex-grow gap-10 p-10",
         jumbo_desc: "md:w-2/3 text-center md:text-left",
         jumbo_header: "text-3xl sm:text-5xl border-b-3 pb-5 mb-5 text-shadow-lg/30",
@@ -45,7 +50,7 @@ function Home() {
         <>
             <header>
                 <HeaderBar />
-                <div className={styles.jumbotron}>
+                <div className={styles.jumbotron} style={{ backgroundImage: `url(${homeJumbotron})` }}>
                 <div className={styles.jumbo_content}>
                     <div className={styles.jumbo_desc}>
                     <h1 className={styles.jumbo_header}>Need Help? We're Here for You</h1>
@@ -64,11 +69,11 @@ function Home() {
                 <section className={styles.section + "bg-white"}>
                     <div className={styles.about_container}>
                         <div className={styles.about_img}>
-                            <img src="src/assets/melinda.jpg" alt="" />
+                            <img src={melindaJpg} alt="" />
                         </div>
                         <div>
                             <h1 className={styles.about_title}>Dr. Melinda</h1>
-                            <p className={styles.about_desc}>Dr. Melinda has developed a great reputation as a family dentist. She is also very passionate about dentistry and understands the importance of offering quality dental care to both adults and children. She will ensure you receive the highest standards of dental care in a good communication so patient will understand why it’s important and how to maintain oral hygiene.</p>
+                            <p className={styles.about_desc}>Dr. Melinda has developed a great reputation as a family dentist. She is also very passionate about dentistry and understands the importance of offering quality dental care to both adults and children. She will ensure you receive the highest standards of dental care in a good communication so patient will understand why it's important and how to maintain oral hygiene.</p>
                         </div>
                     </div>
                 </section>
