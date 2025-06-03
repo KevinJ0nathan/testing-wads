1106821069d8469593fb1144c9113415cf5f5477
Kevin Jonathan <realkevinjonathan@gmail.com>
2025-06-03 11:46:08 +0700

    skibidi

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    // Add other directories here if needed, e.g., "./src/layouts/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
